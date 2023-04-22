203f78d9748633b9735a1ffcbef57ca024b2e3df
nicalebis <98501090+nicalebis@users.noreply.github.com>
2023-04-22 12:59:41 -0400

    Create probes-shapes-small.js

---
+++ probes-shapes-small.js
@@ -0,0 +1,192 @@
+const probeSize = 6;
+
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getContrastingColor(r, g, b) {
+  return `rgb(${255 - r}, ${255 - g}, ${255 - b})`;
+}
+
+function drawProbe(ctx, size, x, y, shape, bgData) {
+  const data = bgData.data;
+  let r = 0;
+  let g = 0;
+  let b = 0;
+  let count = 0;
+
+  for (let i = 0; i < data.length; i += 4) {
+    r += data[i];
+    g += data[i + 1];
+    b += data[i + 2];
+    count++;
+  }
+
+  r /= count;
+  g /= count;
+  b /= count;
+
+  const probeColor = getContrastingColor(r, g, b);
+
+  ctx.strokeStyle = 'rgba(0, 0, 0, 0)';
+  ctx.beginPath();
+  ctx.arc(x, y, 15, 0, 2 * Math.PI);
+  ctx.stroke();
+
+  ctx.fillStyle = probeColor;
+  if (shape === 'square') {
+    ctx.fillRect(x - size / 2, y - size / 2, size, size);
+  } else if (shape === 'triangle') {
+    ctx.beginPath();
+    ctx.moveTo(x - size / 2, y + size / 2);
+    ctx.lineTo(x + size / 2, y + size / 2);
+    ctx.lineTo(x, y - size / 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+}
+
+function gaussianRandom(mean, sd) {
+  const u = Math.random();
+  const v = Math.random();
+  const z = Math.sqrt(-2 * Math.log(u)) * Math.cos(2 * Math.PI * v);
+  return Math.round(mean + sd * z);
+}
+
+function clearCanvas(ctx) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+let isVideoScreenVisible = false;
+
+function runTrialWithProbes(videoId, canvasId) {
+  const video = document.getElementById(videoId);
+  const canvas = document.getElementById(canvasId);
+  const ctx = canvas.getContext('2d');
+  let probeVisible = false;
+  let probeTimeout;
+
+  let lastProbeTime;
+  let lastProbeShape;
+  let lastProbeX;
+  let lastProbeY;
+  const results = [];
+
+  function showProbe() {
+    if (probeVisible || !isVideoScreenVisible) return;
+
+    clearCanvas(ctx);
+
+    let x, y, bgData, probeColor, shape;
+    let numBlackPixels = 0;
+    
+    do {
+      x = Math.random() * (ctx.canvas.width - 2 * probeSize) + probeSize;
+      y = Math.random() * (ctx.canvas.height - 2 * probeSize) + probeSize;
+
+      ctx.strokeStyle = 'rgba(0, 0, 0, 0)';
+      ctx.beginPath();
+      ctx.arc(x, y, 10, 0, 2 * Math.PI);
+      ctx.stroke();
+
+      bgData = ctx.getImageData(x - 15, y - 15, 30, 30);
+
+      let numBlackPixels = 0;
+      for (let i = 0; i < bgData.data.length; i += 4) {
+        if (bgData.data[i] === 0 && bgData.data[i + 1] === 0 && bgData.data[i + 2] === 0) {
+            numBlackPixels++;
+        }
+      }
+    } while (numBlackPixels > 450);
+
+    shape = Math.random() < 0.5 ? 'square' : 'triangle';
+    drawProbe(ctx, probeSize, x, y, shape, bgData);
+
+    lastProbeTime = video.currentTime;
+lastProbeShape = shape;
+lastProbeX = x;
+lastProbeY = y;
+
+const meanDelay = 1600;
+const sdDelay = 400;
+const delay = Math.max(0, Math.round(gaussianRandom(meanDelay, sdDelay)));
+
+// clear the previous timeout if it exists
+if (probeTimeout) {
+  clearTimeout(probeTimeout);
+}
+
+probeTimeout = setTimeout(showProbe, delay);
+
+probeVisible = true;
+
+}
+
+function hideProbeAndScheduleNext() {
+if (!probeVisible || !isVideoScreenVisible) return;
+
+clearCanvas(ctx);
+probeVisible = false;
+
+const meanDelay = 1600;
+const sdDelay = 400;
+const delay = Math.max(0, Math.round(gaussianRandom(meanDelay, sdDelay)));
+
+// clear the previous timeout if it exists
+if (probeTimeout) {
+  clearTimeout(probeTimeout);
+}
+
+probeTimeout = setTimeout(showProbe, delay);
+
+}
+
+function handleKeyPress(e) {
+  if (!isVideoScreenVisible || !probeVisible) return;
+  if (e.key === 'f' || e.key === 'j') {
+    const currentTime = video.currentTime;
+    const rt = currentTime - lastProbeTime;
+    const keyPressed = e.key;
+    const isCorrect = (keyPressed === 'f' && lastProbeShape === 'square') || (keyPressed === 'j' && lastProbeShape === 'triangle');
+
+    results.push({
+      rt: rt,
+      x: lastProbeX,
+      y: lastProbeY,
+      keyPressed: keyPressed,
+      correct: isCorrect,
+    });
+
+    // Display the last reaction time and whether the last keypress was correct
+    document.getElementById("last-reaction-time").textContent = `Last Reaction Time: ${rt.toFixed(3)} seconds`;
+    document.getElementById("last-keypress-correct").textContent = `Last Keypress Correct: ${isCorrect ? "Yes" : "No"}`;
+
+
+    console.log(results);
+
+    // clear the previous timeout if it exists
+    if (probeTimeout) {
+      clearTimeout(probeTimeout);
+    }
+
+    hideProbeAndScheduleNext();
+  }
+}
+
+
+
+video.addEventListener('play', () => {
+setTimeout(showProbe, 2500);
+});
+
+video.addEventListener('ended', () => {
+clearCanvas(ctx);
+probeVisible = false;
+document.removeEventListener('keydown', handleKeyPress); // Remove keydown event listener when video ends
+});
+
+document.addEventListener('keydown', handleKeyPress);
+}
+
+window.runTrialWithProbes = runTrialWithProbes;
+
